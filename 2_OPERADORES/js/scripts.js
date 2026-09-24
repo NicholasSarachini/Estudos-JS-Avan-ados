99cafbac0f2248d3ce81191d816357fcf11a60d6
Nicholas Rosa Sarachini de Oliveira <ni.oliveira901gmail.com>
2026-09-23 22:04:29 -0300

    Estudos de JS

---
+++ 2_OPERADORES/js/scripts.js
@@ -0,0 +1,126 @@
+// asdsadsa
+
+/*
+sdifbsduifuis
+*/
+
+// 1 - Number
+console.log(2);
+console.log(typeof 2);
+
+console.log(5.14);
+console.log(typeof 5.14);
+
+console.log(-127);
+console.log(typeof -127);
+
+// 2 - Ops. Aritméticas 
+console.log(2 + 4);
+console.log(10 - 5);
+
+console.log(4 * 8);
+console.log(80 / 10);
+
+console.log(2 + (5*5));
+console.log(9 + (9/1));
+
+// 3 - Special Numbers
+console.log(typeof Infinity);
+console.log(typeof infinity);
+
+console.log(typeof -Infinity);
+
+console.log(12 * "ddd");
+
+console.log(typeof NaN);
+
+// 4 - Strings
+console.log("Um texto");
+console.log('Mais um texto');
+console.log(`13`);
+
+console.log(typeof `12`);
+console.log(typeof `Mais um textao`);
+
+// 5 - Símbolos especiais em strings
+console.log("Testando a \nquebra de linha");
+
+console.log("Espaçamento \t de tab");
+
+// 6 - Concatenação
+console.log("Oi, " + "tudo" + " bem?");
+
+console.log(`Testando ` + `com ` + `crase`);
+
+// 7 - template strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+
+console.log(`Podemos executar qualquer coisa aqui ${console.log("teste")}`);
+
+// 8 - Boolean
+console.log(true);
+
+console.log(5 > 20);
+
+console.log(30 > 10);
+
+console.log(typeof false);
+
+// 9 - Comparações
+console.log( 5 <= 5 );
+
+console.log(5 < 5);
+
+console.log (10 == 10);
+
+console.log(10 == 9);
+
+console.log(10 != 9);
+
+// 10 - Idêntico
+console.log( 9 == "9" );
+
+console.log( 9 + "9" );
+
+console.log(9 === "9" );
+
+console.log(9 != "9");
+
+console.log(9 !== "9");
+
+// 11 - Operadores Lógicos
+console.log(true && true);
+
+console.log(true && false);
+
+console.log( 5 > 2 && 2 < 10 );
+
+console.log(5 > 2 && "Nicholas" === 1);
+
+console.log(5 > 2 || "Nicholas" === 1);
+
+console.log(5 < 2 || 5 < 100 );
+
+console.log(!true);
+
+console.log(!5 > 2);
+
+// 12 - Empty values
+console.log(typeof null, typeof undefined);
+
+console.log(null === undefined);
+
+console.log(null == undefined);
+
+console.log(null == false);
+
+console.log(undefined == false);
+
+// 13 - Mudança de tipos
+console.log(5 * null);
+
+console.log("teste" * "opa");
+
+console.log("10" + 1);
+
+console.log("10" - 1);
